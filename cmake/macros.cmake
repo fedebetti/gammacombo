e5369ce50fde83a0423807c2efa97c89623eedf0
Tommaso Pajero <tommaso.pajero@cern.ch>
2024-12-19 19:12:05 +0100

    add pre-commit hook (CMake)

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -1,50 +1,48 @@
-# collection of helpful macros for cmake
-# created by J. Engels, DESY and Ch. Rosemann, DESY
-
+#[[
+Collection of helpful macros for cmake created by
+  * J. Engels (DESY)
+  * Ch. Rosemann (DESY).
+]]
 
 # create symbolic lib target for calling library targets
-MACRO( ADD_SHARED_LIBRARY _name )
-    ADD_LIBRARY( ${_name} SHARED ${ARGN} )
-    
-    # change lib_target properties
-    SET_TARGET_PROPERTIES( ${_name} PROPERTIES
-        # create *nix style library versions + symbolic links
-        VERSION ${${PROJECT_NAME}_VERSION}
-        SOVERSION ${${PROJECT_NAME}_SOVERSION}
-    )
-ENDMACRO( ADD_SHARED_LIBRARY )
+macro(ADD_SHARED_LIBRARY _name)
+  add_library(${_name} SHARED ${ARGN})
 
+  # change lib_target properties
+  set_target_properties(
+    ${_name}
+    PROPERTIES # create *nix style library versions + symbolic links
+               VERSION ${${PROJECT_NAME}_VERSION} SOVERSION
+                                                  ${${PROJECT_NAME}_SOVERSION})
+endmacro(ADD_SHARED_LIBRARY)
 
 # in order to include cmake projects into other projects config files must exist
 # helper macro for generating project configuration file
-MACRO( GENERATE_PACKAGE_CONFIGURATION_FILES )
+macro(GENERATE_PACKAGE_CONFIGURATION_FILES)
 
-    FOREACH( arg ${ARGN} )
-        IF( ${arg} MATCHES "Config.cmake" )
-            IF( EXISTS "${PROJECT_SOURCE_DIR}/cmake/${arg}.in" )
-                CONFIGURE_FILE( "${PROJECT_SOURCE_DIR}/cmake/${arg}.in"
-                                "${PROJECT_BINARY_DIR}/${arg}" @ONLY
-                )
-                INSTALL( FILES "${PROJECT_BINARY_DIR}/${arg}" DESTINATION . )
-             ENDIF()
-        ENDIF()
+  foreach(arg ${ARGN})
+    if(${arg} MATCHES "Config.cmake")
+      if(EXISTS "${PROJECT_SOURCE_DIR}/cmake/${arg}.in")
+        configure_file("${PROJECT_SOURCE_DIR}/cmake/${arg}.in"
+                       "${PROJECT_BINARY_DIR}/${arg}" @ONLY)
+        install(FILES "${PROJECT_BINARY_DIR}/${arg}" DESTINATION .)
+      endif()
+    endif()
 
+    if(${arg} MATCHES "ConfigVersion.cmake")
+      # version configuration file
+      if(EXISTS "${PROJECT_SOURCE_DIR}/cmake/${arg}.in")
+        configure_file("${PROJECT_SOURCE_DIR}/cmake/${arg}.in"
+                       "${PROJECT_BINARY_DIR}/${arg}" @ONLY)
+        install(FILES "${PROJECT_BINARY_DIR}/${arg}" DESTINATION .)
+      endif(EXISTS "${PROJECT_SOURCE_DIR}/cmake/${arg}.in")
+    endif()
 
-        IF( ${arg} MATCHES "ConfigVersion.cmake" )
-            # version configuration file
-            IF( EXISTS "${PROJECT_SOURCE_DIR}/cmake/${arg}.in" )
-                CONFIGURE_FILE( "${PROJECT_SOURCE_DIR}/cmake/${arg}.in"
-                                "${PROJECT_BINARY_DIR}/${arg}" @ONLY
-                )
-                INSTALL( FILES "${PROJECT_BINARY_DIR}/${arg}" DESTINATION . )
-            ENDIF( EXISTS "${PROJECT_SOURCE_DIR}/cmake/${arg}.in" )
-        ENDIF()
+    if(${arg} MATCHES "LibDeps.cmake")
+      export_library_dependencies("${arg}")
+      install(FILES "${PROJECT_BINARY_DIR}/${arg}" DESTINATION lib/cmake)
+    endif()
 
-        IF( ${arg} MATCHES "LibDeps.cmake" )
-            EXPORT_LIBRARY_DEPENDENCIES( "${arg}" )
-            INSTALL( FILES "${PROJECT_BINARY_DIR}/${arg}" DESTINATION lib/cmake )
-        ENDIF()
+  endforeach()
 
-    ENDFOREACH()
-
-ENDMACRO( GENERATE_PACKAGE_CONFIGURATION_FILES )
+endmacro(GENERATE_PACKAGE_CONFIGURATION_FILES)
