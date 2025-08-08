e52415cba70541acc4cf73beb07b936da44a6bdf
Tommaso Pajero <tommaso.pajero@cern.ch>
2025-08-08 11:43:05 +0200

    add ordering and more sensible default values for CLInterval

--- core/include/CLInterval.h
+++ core/include/CLInterval.h
@@ -10,26 +10,44 @@
 
 #include <TString.h>
 
-///
-/// Class that represents a confidence interval
-///
+#include <compare>
+#include <limits>
+
+/**
+ * Class that represents a confidence interval.
+ *
+ * Intervals are ordered based on minmethod, maxmethod, centralmethod, decreasing p-value, increasing min, increasing
+ * max (p-value at maximum and central value should then be uniquely identified by the other characteristics of the
+ * intervals).
+ */
 class CLInterval {
  public:
-  CLInterval();
-  CLInterval(const CLInterval& other);
-  ~CLInterval();
-  void print();
+  bool operator==(const CLInterval& rhs) const noexcept;
+  std::strong_ordering operator<=>(const CLInterval& rhs) const noexcept;
 
-  float pvalue;           // pvalue corresponding to this interval
-  float pvalueAtCentral;  // pvalue at the central value
-  float min;              // lower interval border
-  float max;              // upper interval border
-  float central;          // central value
-  bool minclosed;         // true if the interval was not closed by limited scan range
-  bool maxclosed;         // true if the interval was not closed by limited scan range
-  TString minmethod;      // details on the algorithm that found this interval
-  TString maxmethod;      // details on the algorithm that found this interval
-  TString centralmethod;  // details on the algorithm that found the central value
+  /// Print the details of the CL interval.
+  void print() const;
+
+  /// P-value that defines the interval.
+  double pvalue = std::numeric_limits<double>::quiet_NaN();
+  /// P-value at the central value.
+  double pvalueAtCentral = std::numeric_limits<double>::quiet_NaN();
+  /// Lower interval border.
+  double min = std::numeric_limits<double>::quiet_NaN();
+  /// Upper interval border.
+  double max = std::numeric_limits<double>::quiet_NaN();
+  /// Central value.
+  double central = std::numeric_limits<double>::quiet_NaN();
+  /// True if the interval was not closed by limited scan range.
+  bool minclosed = false;
+  /// True if the interval was not closed by limited scan range.
+  bool maxclosed = false;
+  /// Identifier of the algorithm that found the lower border of the interval.
+  TString minmethod = "n/a";
+  /// Identifier of the algorithm that found the upper border of the interval.
+  TString maxmethod = "n/a";
+  /// Identifier of the algorithm that found the central value of the interval.
+  TString centralmethod = "n/a";
 };
 
 #endif
