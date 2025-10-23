fbe8a34646c14f50cf32ae464a44de3a33a2989d
Federico Betti <federico.betti@cern.ch>
2025-10-23 17:21:11 +0200

    copy Lb2pktaul from tutorial

---
+++ Lb2pktaul/include/PDF_Circle.h
@@ -0,0 +1,31 @@
+/**
+ * Gamma Combination
+ * Author: Till Moritz Karbach, moritz.karbach@cern.ch
+ * Date: July 2014
+ *
+ **/
+
+#ifndef PDF_Circle_h
+#define PDF_Circle_h
+
+#include <PDF_Abs.h>
+
+#include <TString.h>
+
+///
+/// Part of the Lb2pktaul. Just a PDF that defines a circle
+/// in a_gaus and b_gaus.
+///
+class PDF_Circle : public PDF_Abs {
+ public:
+  PDF_Circle(TString cObs = "year2013", TString cErr = "year2013", TString cCor = "year2013");
+  void buildPdf();
+  void initObservables();
+  virtual void initParameters();
+  virtual void initRelations();
+  void setCorrelations(TString c);
+  void setObservables(TString c);
+  void setUncertainties(TString c);
+};
+
+#endif
---
+++ Lb2pktaul/include/ParametersCartesian.h
@@ -0,0 +1,23 @@
+/**
+ * Author: Till Moritz Karbach, moritz.karbach@cern.ch
+ * Date: April 2013
+ *
+ * Abstract class to define the (nuisance) parameters:
+ * Gamma Combination
+ *
+ **/
+
+#ifndef ParametersCartesian_h
+#define ParametersCartesian_h
+
+#include <ParametersAbs.h>
+
+class ParametersCartesian : public ParametersAbs {
+ public:
+  ParametersCartesian();
+
+ protected:
+  void defineParameters();
+};
+
+#endif
