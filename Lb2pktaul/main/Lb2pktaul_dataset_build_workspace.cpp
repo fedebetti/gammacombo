d45c2950112bd4c285d2824e9619dd1edf7868fc
Federico Betti <federico.betti@cern.ch>
2025-10-28 12:32:25 +0100

    first almost-working version of dataset building

--- Lb2pktaul/main/Lb2pktaul_dataset_build_workspace.cpp
+++ Lb2pktaul/main/Lb2pktaul_dataset_build_workspace.cpp
@@ -1,8 +1,12 @@
+#include <RooAbsArg.h>
 #include <RooAddPdf.h>
 #include <RooArgList.h>
+#include <RooCustomizer.h>
 #include <RooDataSet.h>
+#include <RooDstD0BG.h>
 #include <RooExponential.h>
 #include <RooExtendPdf.h>
+#include <RooFit.h>
 #include <RooFitResult.h>
 #include <RooFormulaVar.h>
 #include <RooGaussian.h>
@@ -11,6 +15,10 @@
 #include <RooWorkspace.h>
 
 #include <TCanvas.h>
+#include <TFile.h>
+#include <TString.h>
+
+#include <typeinfo>
 
 int main() {
   /////////////////////////////////////////////////////////
@@ -20,39 +28,115 @@ int main() {
   //
   /////////////////////////////////////////////////////////
 
+  double mL = 4600.0;
+  double mR = 10000.0;
+  RooRealVar Lb_M_reco("Lb_M_reco", "#it{m}(#it{#Lambda_{b}^{0}}) [MeV / #it{c}^{2}]", mL, mR);
+
   // First, define the signal peak of the mass model
 
-  // It consists of a gaussian to describe the signal...
-  RooRealVar mass("mass", "mass", 4360., 6360., "MeV");
-  RooRealVar mean("mean", "mean", 5370);
-  RooRealVar sigma("sigma", "sigma", 20.9);
-  RooGaussian signal_model("g", "g", mass, mean, sigma);
+  // TODO: import signal and bkg pdfs, and bkg yield, from workspaces
+  // TODO: make workspace path and name configurable
+  TString path_input_workspaces("~/work/Lb_to_taus/vrd-lb2pktaumu/hadronic/fit/");  // path to the input workspaces
+  auto file_w_bkg = TFile::Open(path_input_workspaces + "w_bkg_lowBDTCut.root");
+  auto w_bkg = (RooWorkspace*)file_w_bkg->Get("w");
+  std::cout << "INPUT WORKSPACE FOR BKG:\n";
+  w_bkg->Print();
+  auto file_w_sig = TFile::Open(path_input_workspaces + "w_sig_lowBDTCut.root");
+  auto w_sig = (RooWorkspace*)file_w_sig->Get("w");
+  std::cout << "INPUT WORKSPACE FOR SIG:\n";
+  w_sig->Print();
 
-  // ... and of an exponential function to describe the background.
-  RooRealVar exponent("exponent", "exponent", -1e-3, -1., 1.);
-  RooRealVar n_bkg("Nbkg", "Nbkg", 4900, 0, 10000);
-  RooExponential background_model("background_model", "background_model", mass, exponent);
-  RooExponential bkg_only_model("bkg_only_model", "bkg_only_model", mass, exponent);
-  RooExtendPdf extended_bkg_model("extended_bkg_model", "extended_bkg_model", bkg_only_model, n_bkg);
+  // this is needed to map the sig function to the same observable of the bkg
+  auto m = w_sig->var("m");
+  auto sig_tmp = w_sig->pdf("sig");
+  auto m_tmp = sig_tmp->getObservables(RooArgSet(*m))->find("m");
+  RooCustomizer customizer_sig(*sig_tmp, "sig");
+  customizer_sig.replaceArg(*m_tmp, Lb_M_reco);
+  RooAbsArg* sig_rebuilt = customizer_sig.build(true);  // true = recycle unchanged nodes
+  TObject* clone_obj = sig_rebuilt->clone(Form("%s_clone", sig_rebuilt->GetName()));
+  auto* cloned_pdf = dynamic_cast<RooAbsPdf*>(clone_obj);
+  std::unique_ptr<RooAbsPdf> signal_model{cloned_pdf};
+  signal_model->SetNameTitle("signal_model", "signal_model");
+
+  auto bkg_tmp = w_bkg->pdf("bkg");
+  m_tmp = bkg_tmp->getObservables(RooArgSet(*m))->find("m");
+  RooCustomizer customizer_bkg(*bkg_tmp, "bkg");
+  customizer_bkg.replaceArg(*m_tmp, Lb_M_reco);
+  RooAbsArg* bkg_rebuilt = customizer_bkg.build(true);  // true = recycle unchanged nodes
+  TObject* clone_obj_bkg = bkg_rebuilt->clone(Form("%s_clone", bkg_rebuilt->GetName()));
+  auto* cloned_pdf_bkg = dynamic_cast<RooAbsPdf*>(clone_obj_bkg);
+  std::unique_ptr<RooAbsPdf> bkg_only_model{cloned_pdf_bkg};
+  bkg_only_model->SetNameTitle("bkg_only_model", "bkg_only_model");
+
+  // std::cout << "TEST2 " << typeid(signal_model).name() << std::endl;
+  // auto bkg_tmp = w_bkg->pdf("bkg");
+  // m_tmp = bkg_tmp->getObservables(RooArgSet(*m))->find("m");
+  // RooCustomizer customizer_bkg(*bkg_tmp, "bkg");
+  // customizer_bkg.replaceArg(*m_tmp, Lb_M_reco);
+  // auto bkg_only_model = *(dynamic_cast<RooAddPdf*>(customizer.build(true)));  // true = recycle unchanged nodes
+  // signal_model.SetName("signal_model");
+
+  /*  // It consists of three gaussians to describe the signal...
+    RooRealVar mean1("mean1", "mean1", 5627.374838, 5000.0, 6000.0);
+    RooRealVar sigma1("sigma1", "sigma1", 78.19803058, 1e-4, 2000.0);
+    RooGaussian gauss1("gauss1", "gauss1", Lb_M_reco, mean1, sigma1);
+    RooRealVar mean2("mean2", "mean2", 6153.337372, 5500.0, 6500.0);
+    RooRealVar sigma2("sigma2", "sigma2", 689.2604944, 1e-4, 2000.0);
+    RooGaussian gauss2("gauss2", "gauss2", Lb_M_reco, mean2, sigma2);
+    RooRealVar mean3("mean3", "mean3", 5724.880206, 5000.0, 6000.0);
+    RooRealVar sigma3("sigma3", "sigma3", 258.6402997, 1e-4, 2000.0);
+    RooGaussian gauss3("gauss3", "gauss3", Lb_M_reco, mean3, sigma3);
+    RooRealVar f1("f1", "f1", 0.4064624117, 0.0, 1.0);
+    RooRealVar f2("f2", "f2", 0.1255721298, 0.0, 1.0);
+    RooAddPdf signal_model("signal_model", "signal_model", RooArgList(gauss1, gauss2, gauss3), RooArgList(f1, f2),
+    true);
+    */
+  auto params_signal = signal_model->getParameters(RooArgSet(Lb_M_reco));
+  for (auto& param : *params_signal) {
+    if (auto* real = dynamic_cast<RooRealVar*>(param)) { real->setConstant(true); }
+  }
+
+  /*  // ... and of a DstD0BG function to describe the background.
+    RooRealVar m0("m0", "m0", 4503.098360, 4100.0, mL);
+    RooRealVar C("C", "C", 512.6540166, 0.0, 5000.0);
+    // C.setConstant();
+    RooRealVar A("A", "A", -9.7404521505, -50.0, 50.0);
+    // A.setConstant();
+    RooRealVar B("B", "B", 0.0, -0.01, 0.01);
+    B.setVal(0.0);
+    B.setConstant();
+    RooDstD0BG bkg_only_model("bkg_only_model", "bkg_only_model", Lb_M_reco, m0, C, A, B);*/
+  RooRealVar n_bkg("Nbkg", "Nbkg", 4981., 0., 10000.);
+  RooExtendPdf extended_bkg_model("extended_bkg_model", "extended_bkg_model", *bkg_only_model, n_bkg);
 
   // The number of signal events is related to the branching ratio via the formula <branching ratio> = <n_sig> *
   // <normalization factor> The normalization factor is not exactly known. Instead, it has to be estimated. The
   // estimator for the normalization factor is a global observable that constrains its value via a Gaussian Constraint.
-  RooRealVar norm_constant_obs("norm_constant_glob_obs", "global observable of normalization constant", 1e-8, 1e-20,
-                               1e-6);  // this is the observed value, the global observable
-  RooRealVar norm_constant("norm_constant", "norm_constant", 1e-8, 1e-20,
-                           1e-6);  // the normalization constant is a nuisance parameter.
-  RooRealVar norm_constant_sigma("norm_constant_sigma", "norm_constant_sigma", 5e-10);
+  double observedValueGlobalObservable =
+      2.6e-7;  // estimated value of the normalization constant, taken from slide 30 of //
+               // https://indico.cern.ch/event/1527783/contributions/6427746/attachments/3120608/5533689/WG_August20th_VRD_Lbpktaulepton.pdf
+  double sigma_observedValueGlobalObservable =
+      0.1 * observedValueGlobalObservable;  // assuming 10% relative uncertainty
+  RooRealVar norm_constant_obs(
+      "norm_constant_glob_obs", "global observable of normalization constant", observedValueGlobalObservable,
+      observedValueGlobalObservable - 7 * sigma_observedValueGlobalObservable,
+      observedValueGlobalObservable + 7 * sigma_observedValueGlobalObservable);  // this is the observed value, the
+                                                                                 // global observable
+  RooRealVar norm_constant(
+      "norm_constant", "norm_constant", observedValueGlobalObservable - 7 * sigma_observedValueGlobalObservable,
+      observedValueGlobalObservable + 7 * sigma_observedValueGlobalObservable);  // the normalization constant is a
+                                                                                 // nuisance parameter.
+  RooRealVar norm_constant_sigma("norm_constant_sigma", "norm_constant_sigma", sigma_observedValueGlobalObservable);
   RooGaussian norm_constant_constraint("norm_constant_constraint", "norm_constant_constraint", norm_constant_obs,
                                        norm_constant, norm_constant_sigma);
 
   // Now we can build the mass model by adding the signal and background probability density functions
-  RooRealVar branchingRatio("branchingRatio", "branchingRatio", 1e-7, -1e-6,
-                            0.0001);  // this is the branching ratio, the parameter of interest
+  RooRealVar branchingRatio("branchingRatio", "branchingRatio", 1e-9, 0,
+                            1e-5);  // this is the branching ratio, the parameter of interest
   RooFormulaVar n_sig("Nsig", "branchingRatio/norm_constant", RooArgList(branchingRatio, norm_constant));
-  RooExtendPdf extended_sig_model("extended_sig_model", "extended_sig_model", signal_model, n_sig);
+  RooExtendPdf extended_sig_model("extended_sig_model", "extended_sig_model", *signal_model, n_sig);
 
-  RooAddPdf mass_model("mass_model", "mass_model", RooArgList(signal_model, background_model),
+  RooAddPdf mass_model("mass_model", "mass_model", RooArgList(*signal_model, *bkg_only_model),
                        RooArgList(n_sig, n_bkg));
 
   /////////////////////////////////////////////////////////
@@ -64,17 +148,20 @@ int main() {
 
   /////////////////////////////////////////////////////////
   //
-  // In this Lb2pktaul, we generate our data instead of using a real dataset.
-  // We generate one mass dataset with 100 events.
-  // We also generate a single measurement of the global observable.
-  double observedValueGlobalObservable = norm_constant_constraint.generate(RooArgSet(norm_constant_obs), 1)
-                                             ->get(0)
-                                             ->getRealValue("norm_constant_glob_obs");
-  //
-  RooDataSet& data =
-      *mass_model.generate(RooArgSet(mass), 5000);  // we use a reference here to avoid copying the object
-  data.SetName("data");  // the name of the dataset MUST be "data" in order for the framework to identify it.
-  //
+  // We create the dataset from real data (Wrong Sign for the moment)
+  // TODO: set configurable Cut
+  TString data_file_name =
+      "/eos/lhcb/user/f/fbetti/Lb_to_taus/data/Run2/Lb_pKtaue_3pi_SS_PplusEplus_massConstr_sortPi_vetoes_BDT.root";
+  RooRealVar TMVAClassification_BDT_all_noIPCHI2rew("TMVAClassification_BDT_all_noIPCHI2rew",
+                                                    "TMVAClassification_BDT_all_noIPCHI2rew", -1.0, 1.0);
+  RooDataSet data_tmp("data_tmp", "data_tmp", RooArgSet(Lb_M_reco, TMVAClassification_BDT_all_noIPCHI2rew),
+                      RooFit::ImportFromFile(data_file_name, "DecayTree"),
+                      RooFit::Cut("TMVAClassification_BDT_all_noIPCHI2rew>-0.05"));  // the name of the dataset MUST be
+                                                                                     // "data" in order for the
+                                                                                     // framework to identify it.
+  RooDataSet data = *(dynamic_cast<RooDataSet*>(data_tmp.reduce(RooFit::SelectVars(RooArgSet(Lb_M_reco)))));
+  data.SetNameTitle("data", "data");
+
   /////////////////////////////////////////////////////////
 
   // Set the global observables to the observed values and make them constant for the fit.
@@ -91,7 +178,7 @@ int main() {
   //
   /////////////////////////////////////////////////////////
 
-  RooPlot* plot = mass.frame();
+  RooPlot* plot = Lb_M_reco.frame();
   data.plotOn(plot);
   extended_bkg_model.plotOn(plot, RooFit::LineColor(kRed));
   mass_model.plotOn(plot);
@@ -114,10 +201,13 @@ int main() {
   RooArgSet global_observables_set(norm_constant_obs, "global_observables_set");
 
   // one containing the normal Observables (the bin variables in the datasets usually) and
-  RooArgSet dataset_observables_set(mass, "datasetObservables");
+  RooArgSet dataset_observables_set(Lb_M_reco, "datasetObservables");
 
   // one containing the parameters
-  RooArgSet parameters_set(branchingRatio, norm_constant, exponent, n_bkg, "parameters");
+  auto params = mass_model.getParameters(RooArgSet(Lb_M_reco));
+  params->remove(constraint_set);
+  params->remove(global_observables_set);
+  RooArgSet parameters_set(*params, "parameters");
 
   /////////////////////////////////////////////////////////
   //
@@ -127,7 +217,7 @@ int main() {
 
   RooWorkspace workspace("dataset_workspace");
   workspace.import(mass_model);
-  workspace.import(extended_bkg_model);
+  workspace.import(extended_bkg_model, RooFit::RenameConflictNodes("_internal"));  // TODO check if this is used
   workspace.import(data);
   workspace.import(rooFitResult, "data_fit_result");  // this MUST be called data_fit_result
   workspace.defineSet("constraint_set", constraint_set, true);
